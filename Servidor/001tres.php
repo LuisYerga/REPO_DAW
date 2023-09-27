2c26b321f6c7d804e5faa57d0403d3b44f02c185
Luis Yerga <lyergamayor1@gmail.com>
2023-09-27 13:06:44 +0200

    Ej1

---
+++ Servidor/001tres.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "<p>Estos son distintos ejemplos</p>";
+
+
+print ("<p>De mostrar contenido en PHP</p>");
+
+
+
+//Comentario de una linea
+
+/*
+Comentario en bloque
+puedes escribir en dos lineas
+o más
+*/
+?>
+
+<?="<p>Hola Olga</p>"?>
