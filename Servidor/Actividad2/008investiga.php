5f713b0ce495ae1d22c359e6b9f37bce606e3e65
LuisYerga <lyergamayor1@gmail.com>
2023-09-28 17:12:59 +0200

    Ej8

--- Servidor/Actividad2/008investiga.php
+++ Servidor/Actividad2/008investiga.php
@@ -4,4 +4,16 @@
 disponible desde PHP8 (https://www.php.net/manual/es/control-structures.match.php). 
 Explica con un par de lineas su propósito y mediante código demuestra su uso. */
 
-/* */
+/*Match nos es util para buscar la expresión que es extrictamente igual a nuestra condición. A cada opción del match le asignaremos un valor 
+que devolvera y quedara guardado. */
+
+$numero = 3;
+
+$mensaje = match ($numero) {
+     1 => "La variable es 1",
+     2 => "La variable es 2",
+     3 => "La variable es 3",
+     default => "No coincide con los parametros",
+};
+
+echo $mensaje;
