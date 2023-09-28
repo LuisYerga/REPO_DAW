b305aa784208966ba1631cda2347648f7032132c
Luis Yerga <lyergamayor1@gmail.com>
2023-09-28 12:46:28 +0200

    Creación 8

---
+++ Servidor/Actividad2/008investiga.php
@@ -0,0 +1,7 @@
+<?php
+
+/*Investiga para qué sirve la instrucción match(), 
+disponible desde PHP8 (https://www.php.net/manual/es/control-structures.match.php). 
+Explica con un par de lineas su propósito y mediante código demuestra su uso. */
+
+/* */
