d173ee84793f5b2e60efc84eb5dfb72530ea5820
LuisYerga <lyergamayor1@gmail.com>
2023-10-02 23:00:42 +0200

    Ej18 casifin

--- Servidor/Actividad2/018imprimeTique.php
+++ Servidor/Actividad2/018imprimeTique.php
@@ -1,1 +1,70 @@
 <?php
+/*Tras leer los datos del tique de compra, enumera en una tabla los productos, con su precio en euros y pesetas,
+ y finalmente, en una última fila, totalizar en ambas monedas.*/
+
+$productos=["Leche"=>5.48 , "Galletas"=>1.50 , "Batidos"=>2.46];
+$cantLeche=$_GET["Leche"];
+$cantGalletas=$_GET["Galletas"];
+$cantBat=$_GET["Batidos"];
+
+foreach($productos as $nombre => $precio){
+    echo "Una unidad de $nombre cuesta $precio <br/>";
+};
+
+
+echo "<table border=1>";  //A través de echos damos forma a la tabla con la estructura html
+echo "<tr>";
+echo "<th>Producto</th>";
+echo "<th>Precio Euros</th>";
+echo "<th>Precio Pesetas</th>";
+echo "<th>Cantidad</th>";
+echo "<th>Total Euros</th>";
+echo "<th>Total Pesetas</th>";
+echo "</tr>";
+foreach ($productos as $nombre => $precio){
+    echo "<tr>";
+    echo "<td> $nombre </td>"; 
+    echo "<td> $precio </td>"; 
+    $precioPes=$precio*166.39;
+    echo "<td> $precioPes </td>"; 
+
+    if ($nombre == "Leche") {
+        echo "<td>$cantLeche</td>";
+        $totalEuros = $precio * $cantLeche;
+        $sumaEuros= $sumaEuros + $totalEuros;
+        $totalPesetas = $precioPes * $cantLeche;
+        $sumaPes= $sumaPes + $totalPesetas;
+    } elseif ($nombre == "Galletas") {
+        echo "<td>$cantGalletas</td>";
+        $totalEuros = $precio * $cantGalletas;
+        $sumaEuros= $sumaEuros + $totalEuros;
+        $totalPesetas = $precioPes * $cantGalletas;
+        $sumaPes= $sumaPes + $totalPesetas;
+    } elseif ($nombre == "Batidos") {
+        echo "<td>$cantBat</td>";
+        $totalEuros = $precio * $cantBat;
+        $sumaEuros= $sumaEuros + $totalEuros;
+        $totalPesetas = $precioPes * $cantBat;
+        $sumaPes= $sumaPes + $totalPesetas;
+    }
+
+    echo "<td>$totalEuros</td>";
+    echo "<td>$totalPesetas</td>";
+    echo "</tr>";
+}
+echo "<tr>";
+echo "<td></td>";
+echo "<td></td>";
+echo "<td></td>";
+echo "<td></td>";
+echo "<td>Total Compra Euros</td>";
+echo "<td>Total Compra Pesetas</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td></td>";
+echo "<td></td>";
+echo "<td></td>";
+echo "<td></td>";
+echo "<td>$sumaEuros</td>";
+echo "<td>$sumaPes</td>";
+echo "</tr>";
--- Servidor/Actividad2/018preparaTique.php
+++ Servidor/Actividad2/018preparaTique.php
@@ -2,5 +2,20 @@
 /*A partir de una cantidad de productos, leer el nombre y coste de la cantidad de 
 productos indicados (la cantidad, se recibe mediante un parámetro GET vía URL). */
 
-$cantidad=$_GET["cant"];
+$productos=["Leche"=>5.48 , "Galletas"=>1.50 , "Batidos"=>2.46];
 
+$cantLeche=$_GET["cant1"];
+$cantGalletas=$_GET["cant2"];
+$cantBat=$_GET["cant3"];
+
+foreach($productos as $nombre => $precio){
+    echo "Una unidad de $nombre cuesta $precio <br/>";
+};
+?>
+
+<form action="018imprimeTique.php" method="get">
+ <input type="hidden" id="Leche" name="Leche" value="<?php echo $cantLeche;?>" />
+ <input type="hidden" id="Galletas" name="Galletas" value="<?php echo $cantGalletas;?>" />
+ <input type="hidden" id="Batidos" name="Batidos" value="<?php echo $cantBat;?>" />
+ <input type="submit" value="Hacer Tiquet" />
+</form>
