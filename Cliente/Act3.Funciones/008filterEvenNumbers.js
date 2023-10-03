bf0bb53b84cf3217e1c150d41f03e18df3fbcb2b
Luis Yerga <lyergamayor1@gmail.com>
2023-10-03 13:35:52 +0200

    008

---
+++ Cliente/Act3.Funciones/008filterEvenNumbers.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/*Escribe una función que tome un 
+array de números y devuelva un nuevo array que solo contenga los números pares. 
+Utiliza una función flecha para realizar esta operación. */
+
+let numeros = ['1','2','3','4','5','6','7','8','9','10'];
+
+
+let resultado= (array) => {
+     
+}
