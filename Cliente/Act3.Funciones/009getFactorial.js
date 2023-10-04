4014c66c20262ef7ded329d8a614f4c023bde48d
Luis Yerga <lyergamayor1@gmail.com>
2023-10-04 10:42:32 +0200

    Ej 9

--- Cliente/Act3.Funciones/009getFactorial.js
+++ Cliente/Act3.Funciones/009getFactorial.js
@@ -3,13 +3,17 @@
 /*Escribe una función que tome un número entero positivo como argumento y devuelva su factorial.
 El factorial de un número es el producto de todos los enteros positivos desde 1 hasta ese número. */
 
-let numEntero=8;
+let numEntero=1;
 
 let resultado;
 
-let operacion= (num) => {
-    return num * operacion(num-1);
+let operacion= (num) => { //En nuestra función entra el número que hemos introducido
+    if (num===1){  //Cuando num alcance el valor 1 lo devlveremos
+        return 1;
+    }
+    return num * operacion(num-1); //Haremos llamadas recursivas al número inmediatamente anterior hasta llegar a 1. Cuando esto ocurra se comenzarán a realizar las operaciones hasta llegar a nuestro número original
+    //Ejemplo: num(2)*operacion(1)/ Siguiente num(3)*operacion(2)/ Siguiente num(4)*operacion(6)....
 };
 
-numEntero >0 ? resultado=operacion(numEntero) : false;
+numEntero >0 ? resultado=operacion(numEntero) : false; //Si el numero es mayor a 0 entramos a la función
 console.log(resultado);
---
+++ Cliente/Act3.Funciones/010findLongestSubstring.js
@@ -0,0 +1,4 @@
+"use strict"
+
+/*Crea una función que tome una cadena de texto y encuentre la subcadena más larga sin caracteres repetidos.*/
+
