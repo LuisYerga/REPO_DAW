77ef88a526382a2e4fb52a9b1a9e7bc152412724
Luis Yerga <lyergamayor1@gmail.com>
2023-10-04 11:06:38 +0200

    idea 10

--- Cliente/Act3.Funciones/010findLongestSubstring.js
+++ Cliente/Act3.Funciones/010findLongestSubstring.js
@@ -2,3 +2,11 @@
 
 /*Crea una función que tome una cadena de texto y encuentre la subcadena más larga sin caracteres repetidos.*/
 
+let cadena ="Esto es una cadena de texto de ejemplo";
+
+let separador = (cadena) =>{
+    /*for {
+        
+    } */
+    /*for que se pare cuando encuentre coincidencia, guarde longitud en variable temporal y compare con variable final */
+};
