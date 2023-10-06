0fe630d62f21057f6b2a60d31720e2d14f7a1209
Luis Yerga <lyergamayor1@gmail.com>
2023-10-06 09:23:19 +0200

    004EmpConst

--- Servidor/Actividad2_Parte2/004EmpleadoConstante.php
+++ Servidor/Actividad2_Parte2/004EmpleadoConstante.php
@@ -1,17 +1,17 @@
 <?php
 
-/*Copia la clase del ejercicio anterior y modifícala. Elimina los setters de nombre y apellidos, de manera que 
-dichos datos se asignan mediante el constructor (utiliza la sintaxis de PHP8). Si el constructor recibe un tercer parámetro, 
-será el sueldo del Empleado. Si no, se le asignará 1000€ como sueldo inicial.*/
+/*Copia la clase del ejercicio anterior y modifícala. Añade una constante SUELDO_TOPE con 
+el valor del sueldo que debe pagar impuestos, y modifica el código para utilizar la constante.*/
 
 class Empleado{
+    const SUELDO_TOPE = 3333;
     public function __construct (  // Declaramos nuestro constructor 
         protected String $nombre,
         protected String $apellidos,
         protected float $sueldo=1000.0,
         private array $telefono
     ){}
-
+    
     public function getNombre(){  //Creamos getters y setters para manejar la información de los parámetros
         return $this-> nombre;
     }
@@ -25,13 +25,9 @@ class Empleado{
     public function getNombreCompleto() : String {  //Metodo que forma el nombre entero del empleado.Solo devuelve Strings
         return $this->nombre ." ". $this->apellidos;
     }
-    
-    public function getNombreCompleto() : String {  //Metodo que forma el nombre entero del empleado.Solo devuelve Strings
-        return $this->nombre ." ". $this->apellidos;
-    }
 
     public function debePagarImpuestos():bool{  //Metodo que devuelve un boolean true si el sueldo es mayor al numero o false en caso contrario
-        return $this->sueldo > 3333;
+        return $this->sueldo > self::SUELDO_TOPE;
     }
     public function anyadirTelefono($telefono):void{  //Añadimos telefonos al array
         $this->telefono[]=$telefono;
@@ -47,7 +43,7 @@ class Empleado{
     }
 }
 
-$empleado1 = new Empleado("Luis", "Yerga Mayor", 1000,[674858483]);  //Creamos un nuevo empleado con sus datos
+$empleado1 = new Empleado("Luis", "Yerga Mayor", 4000,[674858483]);  //Creamos un nuevo empleado con sus datos
 echo "El nombre completo del empleado es " . $empleado1->getNombreCompleto(). "</br>";  //Llamamos al metodo nombreCompleto
 
 $impuestos = $empleado1->debePagarImpuestos() ? "El empleado " . $empleado1->getNombre() . " debe pagar" : "El empleado " . $empleado1->getNombre() . " no debe pagar";
