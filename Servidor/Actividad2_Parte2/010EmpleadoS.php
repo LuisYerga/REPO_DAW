0d98e609fb303e60351e1bbc35ceaab35b02606d
Luis Yerga <lyergamayor1@gmail.com>
2023-10-09 10:26:23 +0200

    010 por revisar

---
+++ Servidor/Actividad2_Parte2/010EmpleadoS.php
@@ -0,0 +1,93 @@
+<?php
+include("./010PersonaS.php");//include para acceder al archivo con el padre
+
+class Empleado extends Persona{  //Creamos la herencia de Persona en Empleado
+    private static float $sueldoTope = 3333.0;  //Variable estática
+    public function __construct (  // Declaramos nuestro constructor 
+        String $nombre,
+        String $apellidos,
+        int $edad,
+        private array $telefono,
+        protected ? float $sueldo=1000.0
+    ){
+        parent::__construct($nombre, $apellidos, $edad);  //Llamamos a las propiedades heredadas
+    }
+
+    public function getSueldo(): float{
+        return $this-> sueldo;
+    }
+    
+    public function setSueldo($sueldo) :float{ 
+        $this->sueldo=$sueldo;
+        return $this;
+    }
+
+    public static function getSueldoTope(): float{ //Gets y sets de la variable estática de sueldo tope
+        return self::$sueldoTope;
+    }
+
+    public static function setSueldoTope($sueldoTope) :float{
+        self::$sueldoTope=$sueldoTope;
+    }
+
+    public function debePagarImpuestos():bool{  //Metodo que devuelve un boolean true si el sueldo es mayor a la constante
+        return $this->edad > 21 && $this->sueldo > self::$sueldoTope;  //Accedemos a la const por haciendo referencia a la clase (Empleado:: == self::)
+    }
+    public function anyadirTelefono($telefono):void{  //Añadimos telefonos al array
+        $this->telefono[]=$telefono;
+    }
+
+    public function listarTelefonos():String{  //Devolvemos un string del array mediante el metodo implode
+        return implode("," , $this->telefono);
+    }
+
+    public function getTelefonos(): array{  //Añadimos un getTelefonos para sacar el array
+        return $this-> telefono;
+    }
+
+    public function vaciarTelefonos():void{
+        $this->telefono = array();  //Vaciamos el array guardando uno vacío por encima
+    }
+
+    public static function toHtml (Persona $p):String{
+        if($p instanceof Empleado){
+        $html = "<p>";  //Vamos guardando la estructura html en una variable para devolverla al final de la función
+        $html .= "El nombre completo es " .$p->getNombreCompleto(). " y su sueldo es de ". $p->getSueldo() . "</br>";
+        $html .= "Lista Telefonos";
+        $html .= "<ol>"; //Lista ordenada
+        $listaTelf= $p->getTelefonos();  //Obtenemos el array de numeros
+        foreach($listaTelf as $tlf){  //Recorremos uno a uno los elementos del array
+            $html.= "<li>" .$tlf . "</li>";
+        }
+        $html.="</ol>";
+        $html .= "</p>";
+    }
+        return $html;   
+    }
+
+    public function mostrarInfo(Persona $p):String {
+        if($p instanceof Empleado){
+            $html= "<p>";
+            $html .= $impuestos;
+            $html .= "El sueldo tope es " . $this->getSueldoTope();
+            $html .= "La lista de telefonos del empleado es " . $this->listarTelefonos();
+            $html .= "</p>";
+        }
+        return $html;
+    }
+}
+
+
+
+$empleado1 = new Empleado("Luis","Yerga Mayor", 22 ,[674858483],3000.0);  //Creamos un nuevo empleado con sus datos
+
+$impuestos = $empleado1->debePagarImpuestos() ? "El empleado " . $empleado1->getNombre() . " debe pagar" : "El empleado " . $empleado1->getNombre() . " no debe pagar";
+//Guardamos un mensaje diferente dependiendo del valor del boolean que devuelva el metodo.
+
+$empleado1->anyadirTelefono(43); //Añadimos un telefono al array
+
+$html1 = Empleado::toHtml($empleado1);  //Llamamos a la función estática toHtml y guardamos en la variable lo que devuelva. IMPORTANTE mostrar la lista antes de vaciar los teléfonos más abajo(Parece muy obvio a que si...)
+echo $html1;
+
+$html2 = Empleado::mostrarInfo($empleado1);
+echo $html2;
--- Servidor/Actividad2_Parte2/010PersonaS.php
+++ Servidor/Actividad2_Parte2/010PersonaS.php
@@ -1,9 +1,8 @@
 <?php
 
-/*Copia las clases del ejercicio anterior y modifícalas.Añade en Persona un atributo edad
-A la hora de saber si un empleado debe pagar impuestos, lo hará siempre y cuando tenga más de 21
-años y dependa del valor de su sueldo. Modifica todo el código necesario para mostrar y/o editar la edad cuando sea necesario*/
-
+/*Copia las clases del ejercicio anterior y modifícalas.Añade nuevos métodos que hagan una representación
+de todas las propiedades de las clases Persona y Empleado, de forma similar a los realizados en HTML, pero
+sin que sean estáticos, de manera que obtenga los datos mediante $this*/
 
 class Persona{  //Creamos la clase persona con los atributos y metodos que heredará Empleado
     public function __construct(
