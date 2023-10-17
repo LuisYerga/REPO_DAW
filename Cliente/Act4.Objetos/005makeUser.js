7919521b51ed81ef0d6928005923b28aaf02a836
LuisYerga <lyergamayor1@gmail.com>
2023-10-17 22:58:32 +0200

    005

---
+++ Cliente/Act4.Objetos/005makeUser.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/*Aquí la función makeUser devuelve un objeto. ¿Cuál es el resultado de acceder a su atributo ref? ¿Por qué?
+
+function makeUser() {
+  return {
+    name: "John",
+    ref: this
+  };
+}
+let user = makeUser();
+alert( user.ref.name ); // ¿Cuál es el resultado?*/
+
+function makeUser() {
+    return {
+      name: "John",
+      ref: this
+    };
+}
+
+let user = makeUser();
+alert( user.ref.name ); 
+
+//No se devuelve nada en un principio. Esto ocurre porque ref se refiere al propio objeto, guardando su nombre el cual es undefined.
