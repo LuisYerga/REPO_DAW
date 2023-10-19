23009462d104284f4d2e3e32994ddf93dd2a619e
LuisYerga <lyergamayor1@gmail.com>
2023-10-19 19:38:53 +0200

    016 Terminado

--- Cliente/Act4.Objetos/016fixInfinityLoop.js
+++ Cliente/Act4.Objetos/016fixInfinityLoop.js
@@ -11,3 +11,6 @@ while (i != 10) {
   i += 0.2;
   alert(i);
 }
+
+/*A lo largo del bucle se va aumentando la cantidad de i. Sin embargo conforme se avance en la suma se darán una serie de imprecisiones acabando el numero 
+por tener el valor 9.99999, no cumpliendose el requisito necesario para salir. Una posíble solución es limitar el numero de decimales para que sea mas fácil realizar la suma*/
