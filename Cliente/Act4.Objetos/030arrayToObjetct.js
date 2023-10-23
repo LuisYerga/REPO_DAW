4c93198ff6d05f967dcd676e3a612db3a01764d0
LuisYerga <lyergamayor1@gmail.com>
2023-10-23 19:01:59 +0200

    030 fin

--- Cliente/Act4.Objetos/030arrayToObjetct.js
+++ Cliente/Act4.Objetos/030arrayToObjetct.js
@@ -6,7 +6,7 @@ realmente útil cuando trabajamos con información del servidor.
 Para esta actividad asumimos que cada id es único. No existen dos elementos del array con el mismo id.
 Usa el método de array .reduce en la solución.*/
 
-let personajes=[ //Objeto con arrays
+let personajes=[ //Array de objetos
     { id:1,nombre:"Peter Pan", pelicula:"Peter Pan" },
     { id:2,nombre: "Megana", pelicula: "Hércules" },
     { id:5,nombre: "Hades", pelicula: "Hércules" },
@@ -15,10 +15,10 @@ let personajes=[ //Objeto con arrays
 ]
 
 function groupById(arr){ 
-    return arr.reduce((acumulador, personaje)=>{
+    return arr.reduce((acumulador, personaje)=>{  //Iteramos por cada personaje del array agregando como propiedad de acumulador el propio objeto y usando su id como clave. Se devuelve el acumulador una vez contenga todos los objetos del array
         acumulador[personaje.id] = personaje;
-        return acumulador;
-    },{});
+        return acumulador;   //Acumulador debe ser lo que devolvamos a la función
+    },{}); //Establecemos como valor inicial un objeto vacío
 }
 
 let resultado= groupById(personajes);
