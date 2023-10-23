02f2b03353c1c156cc84d8daa272e006dc26138b
LuisYerga <lyergamayor1@gmail.com>
2023-10-23 19:12:04 +0200

    031

---
+++ Cliente/Act4.Objetos/031createDate.js
@@ -0,0 +1,6 @@
+"use strict";
+
+/*Crea un objeto Date para la fecha: Feb 20, 2012, 3:12am. La zona horaria es local. Muéstralo en pantalla utilizando alert.*/
+
+let fecha=new Date(2012,1,20,3,12,0); //año, mes(enero es 0), dia, hora,minuto, segundo
+alert(fecha);
