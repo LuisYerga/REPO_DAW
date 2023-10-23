0d7d1abc3dc2bd349a00ad09b209e0077514a779
Luis Yerga <lyergamayor1@gmail.com>
2023-10-23 14:26:03 +0200

    030 por comentar

--- Cliente/Act4.Objetos/030arrayToObjetct.js
+++ Cliente/Act4.Objetos/030arrayToObjetct.js
@@ -5,3 +5,21 @@ Crea una función groupById(arr) que cree un objeto, con id como clave (key) y l
 realmente útil cuando trabajamos con información del servidor.
 Para esta actividad asumimos que cada id es único. No existen dos elementos del array con el mismo id.
 Usa el método de array .reduce en la solución.*/
+
+let personajes=[ //Objeto con arrays
+    { id:1,nombre:"Peter Pan", pelicula:"Peter Pan" },
+    { id:2,nombre: "Megana", pelicula: "Hércules" },
+    { id:5,nombre: "Hades", pelicula: "Hércules" },
+    { id:4,nombre: "Campanilla", pelicula: "Peter Pan" },
+    { id:3,nombre: "Wendy", pelicula: "Peter Pan" }
+]
+
+function groupById(arr){ 
+    return arr.reduce((acumulador, personaje)=>{
+        acumulador[personaje.id] = personaje;
+        return acumulador;
+    },{});
+}
+
+let resultado= groupById(personajes);
+console.log(resultado);
