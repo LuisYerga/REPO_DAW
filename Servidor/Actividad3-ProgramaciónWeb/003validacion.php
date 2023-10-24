f92b9866baf92f4cf9151bd1a3dcdbd0191a8a49
LuisYerga <lyergamayor1@gmail.com>
2023-10-24 20:24:31 +0200

    003 creacion

---
+++ Servidor/Actividad3-ProgramaciónWeb/003validacion.php
@@ -0,0 +1,6 @@
+<?php
+
+/*A partir del formulario anterior, introduce validaciones en HTML mediante el atributo required de los campos (uso los tipos adecuados para cada campo), y en 
+comprueba los tipos de los datos y que cumplen los valores esperados (por ejemplo, en los checkboxes que los valores recogidos forman parte de todos los posibles).
+Puedes probar a pasarle datos erróneos vía URL y comprobar su comportamiento. Tip: Investiga el uso de la función filter_var.*/
+
