422c9caa145baf9284187940458f27d0d10f89a3
Luis Yerga <lyergamayor1@gmail.com>
2023-10-24 13:32:53 +0200

    002 sin terminar

---
+++ Servidor/Actividad3-ProgramaciónWeb/002formulario.php
@@ -0,0 +1,7 @@
+<?php
+
+/*Crea un formulario que solicite:
+Nombre y apellidos. Email. URL página personal. Sexo (radio). Número de convivientes en el domicilio. Aficiones (checkboxes) – poner mínimo 4 valores.
+Menú favorito (lista selección múltiple) – poner mínimo 4 valores.
+Muestra los valores cargados en una tabla-resumen*/
+
