a97a4e85050716dd081e3cca69b447733044f513
Luis Yerga <lyergamayor1@gmail.com>
2023-10-24 12:58:20 +0200

    001 Creo que esta bien(No entiendo el enunciado)

--- Servidor/Actividad3-ProgramaciónWeb/001server.php
+++ Servidor/Actividad3-ProgramaciónWeb/001server.php
@@ -5,3 +5,5 @@ Prueba a pasarle parámetros por GET (y a no pasarle ninguno).
 Prepara un formulario (001post.html) que haga un envío por POST y compruébalo de nuevo.
 Crea una página (001enlace.html) que tenga un enlace a 001server.php y comprueba el valor de HTTP_REFERER.*/
 
+echo $_SERVER["HTTP_REFERER"] ."<br>";
+
