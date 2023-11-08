fde0d0a1a390bcae66fbac704970d758f4d00182
Luis Yerga <lyergamayor1@gmail.com>
2023-11-08 09:50:57 +0100

    005 final

--- Servidor/Actividad3-ProgramaciónWeb/005subidaImagen.php
+++ Servidor/Actividad3-ProgramaciónWeb/005subidaImagen.php
@@ -6,11 +6,38 @@ En el caso de subir el tipo correcto, visualizar la imagen con el tamaño de anc
 
 if(isset($_FILES["archivo"])){
     $fichero= $_FILES["archivo"];
+    $altura = isset($_POST["altura"]) ? $_POST["altura"] : '';
+    $anchura = isset($_POST["anchura"]) ? $_POST["anchura"] : '';
 }
-if(isset($_POST["altura"])){
-    $altura= $_POST["altura"];
-}
-if(isset($_POST["anchura"])){
-    $anchura= $_POST["anchura"];
+
+$arrayExtensions=['jpg','png', 'gif', 'svg'];
+$checkExtensions=pathinfo($fichero['name'], PATHINFO_EXTENSION);
+if(in_array(strtolower($checkExtensions),$arrayExtensions)){
+    $ruta='img/'.$fichero['name'];
+    move_uploaded_file($fichero['tmp_name'], $ruta);
+    if(isset($ruta) && file_exists($ruta)){
+    echo '<img src="'.$ruta.'" alt="img" height="'.$altura.'" width="'.$anchura.'">';
+    }else{
+        echo "Error al mostra el archivo";
+    }
+}else{
+    echo "Esto no es una imagen válida";
+    ?>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+    <p>
+        <label for="archivo"> Subir archivo </label>
+        <input type="file"  name="archivo" id="archivo" required> <br> <!--Aceptamos solo imagenes-->
+        </p>
+        <p>
+        <label for="altura">Altura</label>
+        <input type="number" name="altura" id="altura" required> <br>
+        </p>
+        <p>
+        <label for="anchura">Anchura</label>
+        <input type="number" name="anchura" id="anchura" required> <br>
+    </p>
+        <input type="submit" value="Enviar"> <!--Enviamos el formulario-->
+    </form>
+    <?php
 }
 
