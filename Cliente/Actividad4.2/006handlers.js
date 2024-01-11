b7df4643eda0bc5146c14c76ab8be03839d1e806
Luis Yerga <lyergamayor1@gmail.com>
2024-01-11 12:54:35 +0100

    006

--- Cliente/Actividad4.2/006handlers.js
+++ Cliente/Actividad4.2/006handlers.js
@@ -1,5 +1,8 @@
 "use strict";
+let button = document.getElementById('butt');
 
 button.addEventListener("click", () => alert("1"));
 button.removeEventListener("click", () => alert("1"));
 button.onclick = () => alert(2);
+
+//Ambas alertas se muestran al hacer click en el boton
