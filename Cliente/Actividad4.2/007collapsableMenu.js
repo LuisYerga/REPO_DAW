9feaeeb2388917b3a82599aacb4ac182adcdf6a6
Luis Yerga <lyergamayor1@gmail.com>
2024-01-12 14:10:09 +0100

    007

--- Cliente/Actividad4.2/007collapsableMenu.js
+++ Cliente/Actividad4.2/007collapsableMenu.js
@@ -1,4 +1,8 @@
-"use strict";
+"use strict"
 
-let collapse= document.getElementById('title');
+let collapse= document.querySelector('.title'); //Seleccionamos los elementos del html
+let desplegable= document.querySelector('ul');
 
+
+
+collapse.addEventListener("click", () => desplegable.classList.toggle('active')); //Añadimos un listener que cambie las propiedades de la clase en función este sea pulsado
