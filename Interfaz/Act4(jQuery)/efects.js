546aa900a2b7be9b0c716d526917c0368aa910ad
Luis Yerga <lyergamayor1@gmail.com>
2024-01-18 12:47:44 +0100

    arreglo efect

--- Interfaz/Act4(jQuery)/efects.js
+++ Interfaz/Act4(jQuery)/efects.js
@@ -14,14 +14,26 @@ $(document).ready(function(){
     });
 });
 
-$('#galeria').mouseenter(function(){
-    $('#galeria').animate({
+$('#oficina1').mouseenter(function(){
+    $('#oficina1').animate({
+        width: "600px"
+    });
+});
+
+$('#oficina1').mouseleave(function(){
+    $('#oficina1').animate({
+        width: "300px" 
+    });
+});
+
+$('#oficina2').mouseover(function(){
+    $('#oficina2').animate({
         width: "600px" 
     });
 });
 
-$('#galeria').mouseleave(function(){
-    $('#galeria').animate({
+$('#oficina2').mouseout(function(){
+    $('#oficina2').animate({
         width: "300px" 
     });
 });
